4ebd22629cec8c21e85f1e97d908d6b204c3ee58
Konstantin Krastev <grizmin@gmail.com>
2026-06-27 00:29:51 +0300

    dropdown fix

--- calculator/migrations/0008_rename_ginrecipe_recipe.py
+++
@@ -1,5 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-    dependencies = [('calculator', '0007_ginrecipe_water_for_maceration')]
-    operations = [migrations.RenameModel('GinRecipe', 'Recipe')]
